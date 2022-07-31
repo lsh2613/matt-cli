95ebd5651a7e12aa91f96d3363c24b09051ca45a
gajeong <rlarkwjd127@gmail.com>
2022-07-31 23:48:15 +0900

    header css 모듈화, css variable 설정 안됨

--- src/componets/common/header/header.jsx
+++ src/componets/common/header/header.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './header.module.css'
 
 const Header = ({onLogout}) => {
+  const [hover, setHover] = useState(false);
+
   return (
   <header className={styles.header}>
-    <div classNmae={styles.logo}>Mat-t</div>
+    <div className={styles.logo}>Mat-t</div>
+    <div className={styles.menu}>
+      <li className={styles.menuList}>카테고리</li>
+      <li className={styles.menuList}>멘토조회</li>
+    </div>
     {onLogout ? (
-      <button className={styles.btn} onClick={onLogout}>
+      <button
+   
+      onClick={onLogout}>
         로그아웃
       </button>
     ):(
-      <button className={styles.btn} onClick={onLogout}>
+      <button className={hover?`${styles.btn} ${styles.hover}`:styles.btn}
+      onMouseEnter={()=> setHover(true)}
+      onMouseLeave = {()=>setHover(false)}
+      onClick={onLogout}>
         로그인
       </button>
     )}
