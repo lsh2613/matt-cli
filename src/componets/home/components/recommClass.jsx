a9bd7b80ef2e0ac0371c7a8c6b2532dd8ad4d1f1
gajeong <rlarkwjd127@gmail.com>
2022-08-20 20:46:47 +0900

    css 수정

--- src/componets/home/components/recommClass.jsx
+++ src/componets/home/components/recommClass.jsx
@@ -3,45 +3,21 @@ import styles from './recommClass.module.css'
 const RecommClass = (props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>이번주 떠오르는 인기 클래스</div>
+      <div className={styles.title}>🔥이번주 떠오르는 인기 클래스</div>
 
       <div className={styles.cardContainer}>
         <div className={styles.card}>
           <div className={styles.classSt}>진행중</div>
           <div className={styles.classTitle}>
-            리액트릭매직쇼
+            리액트릭매직쇼 우짤래미 저짤래미
           </div>
-          <div className={styles.etc}>김가정 멘토</div>
-          <div className={styles.etc}>2022.08.12 ~ 2022.09.01</div>
-          <div className={styles.etc}>현재 지원 인원 <span className={styles.bold}> 7 / 8명</span></div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.classSt}>진행중</div>
-          <div className={styles.classTitle}>
-            리액트릭매직쇼
+          <div className={styles.contents}>
+            <div className={styles.etc}>김가정 멘토</div>
+            <div className={styles.etc}>2022.08.12 ~ 2022.09.01</div>
+            <div className={styles.etc}>현재 지원 인원 <span className={styles.bold}> 7 / 8명</span></div>
           </div>
-          <div className={styles.etc}>김가정 멘토</div>
-          <div className={styles.etc}>2022.08.12 ~ 2022.09.01</div>
-          <div className={styles.etc}>현재 지원 인원 <span className={styles.bold}> 7 / 8명</span></div>
         </div>
-        <div className={styles.card}>
-          <div className={styles.classSt}>진행중</div>
-          <div className={styles.classTitle}>
-            리액트릭매직쇼
-          </div>
-          <div className={styles.etc}>김가정 멘토</div>
-          <div className={styles.etc}>2022.08.12 ~ 2022.09.01</div>
-          <div className={styles.etc}>현재 지원 인원 <span className={styles.bold}> 7 / 8명</span></div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.classSt}>진행중</div>
-          <div className={styles.classTitle}>
-            리액트릭매직쇼
-          </div>
-          <div className={styles.etc}>김가정 멘토</div>
-          <div className={styles.etc}>2022.08.12 ~ 2022.09.01</div>
-          <div className={styles.etc}>현재 지원 인원 <span className={styles.bold}> 7 / 8명</span></div>
-        </div>
+
       </div>
     </div>
   )
