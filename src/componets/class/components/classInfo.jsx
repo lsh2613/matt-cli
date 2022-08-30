6c6e59cea6e2d0020e898751d82ac0040c8c313e
gajeong <rlarkwjd127@gmail.com>
2022-08-30 17:33:29 +0900

    prettier 적용

--- src/componets/class/components/classInfo.jsx
+++ src/componets/class/components/classInfo.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import styles from './classInfo.module.css';
+import React from 'react'
+import styles from './classInfo.module.css'
 import { fetchClass } from '../../../api/class/class'
-import { useState } from 'react';
+import { useState } from 'react'
 import button from '../../../common/button.module.css'
 class ClassInfo extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       class: '',
       id: 4,
@@ -13,11 +13,11 @@ class ClassInfo extends React.Component {
   }
 
   componentDidMount() {
-    fetchClass(this.state.id).then(res => {
+    fetchClass(this.state.id).then((res) => {
       console.log(res.data)
       this.setState({
-        class: res.data
-      });
+        class: res.data,
+      })
     })
   }
 
@@ -31,21 +31,42 @@ class ClassInfo extends React.Component {
           </section>
 
           <section className={styles.infoGroup}>
-
             <div className={styles.instrutorInfo}>
               <article>👩‍🎓 강사 프로필</article>
-              <aside><label>이름</label>김가정</aside>
-              <aside><label>전공</label>실용음악과</aside>
-              <aside><label>대학교</label>경기대학교</aside>
-              <aside><label>평점</label>⭐ 5점</aside>
+              <aside>
+                <label>이름</label>김가정
+              </aside>
+              <aside>
+                <label>전공</label>실용음악과
+              </aside>
+              <aside>
+                <label>대학교</label>경기대학교
+              </aside>
+              <aside>
+                <label>평점</label>⭐ 5점
+              </aside>
             </div>
             <div className={styles.classInfo}>
-
-              <aside><label>카테고리</label>{this.state.class.category}</aside>
-              <aside><label>기간</label>{this.state.class.startDate} ~ {this.state.class.endDate}</aside>
-              <aside><label>시간</label>{this.state.class.startTime} ~ {this.state.class.endTime}</aside>
-              <aside><label>장소</label>{this.state.class.place}</aside>
-              <aside><label>수강생</label>수강신청한 학생 / {this.state.class.numberOfStudents}</aside>
+              <aside>
+                <label>카테고리</label>
+                {this.state.class.category}
+              </aside>
+              <aside>
+                <label>기간</label>
+                {this.state.class.startDate} ~ {this.state.class.endDate}
+              </aside>
+              <aside>
+                <label>시간</label>
+                {this.state.class.startTime} ~ {this.state.class.endTime}
+              </aside>
+              <aside>
+                <label>장소</label>
+                {this.state.class.place}
+              </aside>
+              <aside>
+                <label>수강생</label>수강신청한 학생 /{' '}
+                {this.state.class.numberOfStudents}
+              </aside>
             </div>
           </section>
           <section className={styles.detailInfo}>
@@ -57,4 +78,4 @@ class ClassInfo extends React.Component {
   }
 }
 
-export default ClassInfo;
+export default ClassInfo
