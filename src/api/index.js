911538d519132726ff81987008c290713f623030
Ga jeong <rlarkwjd127@gmail.com>
2022-09-08 00:05:51 +0900

    세션 잘 만들어짐

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { getCookie } from '../util/cookie'
 // 일반적인 axios 인스턴스 사용시에 사용
 function createInstance(url) {
   return axios.create({
@@ -7,9 +7,17 @@ function createInstance(url) {
   })
 }
 
+function createInstaceWithAuth(url) {
+  return axios.create({
+
+    baseURL: `${process.env.REACT_APP_API_URL}${url}`,
+    withCredentials: true,
+  })
+}
+
 export const user = createInstance('/user')
-export const login = createInstance('/login')
+export const login = createInstaceWithAuth('/login')
 export const class_ = createInstance('/class')
 export const tag = createInstance('/tagInfo')
-export const instructor = createInstance('/instructor')
+export const instructor = createInstaceWithAuth('/instructor')
 export const logout = createInstance('/logout')
