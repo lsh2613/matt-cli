cba68548ead678cfde900e305e644762612ed3f3
gajeong <rlarkwjd127@gmail.com>
2022-10-04 12:22:10 +0900

    router 세션 확인 후 , redirect 처리

--- src/app.jsx
+++ src/app.jsx
@@ -8,29 +8,58 @@ import UpdateClass from './componets/instructor/class/updateclass'
 import Waiting from './componets/instructor/class/wating'
 import ClassInfoPage from './componets/class/classInfoPage'
 import AuthInstructor from './componets/instructor/auth/authInstructor'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import SearchPage from './componets/search/searchPage'
 import InstructorPage from './componets/instructor/instructorPage'
 import InstructorInfo from './componets/instructor/components/instructorInfo'
+import { useSelector } from 'react-redux'
 import ClassPage from './componets/class/classPage'
 
 function App() {
+  const login = useSelector((state) => state.user.login)
+
   return (
     <div className={styles.app}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<Signup />} />
-        <Route path='mypage' element={<MyPage />} />
-        <Route path='/makeclass' element={<MakeClass />} />
-        <Route path='/updateclass/:classId' element={<UpdateClass />} />
         <Route path='/class' element={<ClassPage />} />
         <Route exact path='/class/:classId' element={<ClassInfoPage />} />
-        <Route exact path='/class/:classId/waiting' element={<Waiting />} />
         <Route exact path='/instructor' element={<InstructorPage />} />
         <Route exact path='/instructor/:insId' element={<InstructorInfo />} />
-        <Route path='/instructor/auth' element={<AuthInstructor />} />
         <Route path='/search' element={<SearchPage />} />
+
+        {login ? (
+          <>
+            <Route path='mypage' element={<MyPage />} />
+            <Route path='/makeclass' element={<MakeClass />} />
+            <Route path='/updateclass/:classId' element={<UpdateClass />} />
+            <Route exact path='/class/:classId/waiting' element={<Waiting />} />
+            <Route path='/instructor/auth' element={<AuthInstructor />} />
+          </>
+        ) : (
+          <>
+            <Route path='/mypage' element={<Navigate replace to='/login' />} />
+            <Route
+              path='/makeclass'
+              element={<Navigate replace to='/login' />}
+            />
+            <Route
+              path='/updateclass/:classId'
+              element={<Navigate replace to='/login' />}
+            />
+            <Route
+              exact
+              path='/class/:classId/waiting'
+              element={<Navigate replace to='/login' />}
+            />
+            <Route
+              path='/instructor/auth'
+              element={<Navigate replace to='/login' />}
+            />
+          </>
+        )}
       </Routes>
     </div>
   )
