57f0987c5f7e6a7ac64c446d2130f57beb6d11ea
gajeong <rlarkwjd127@gmail.com>
2022-10-05 11:42:24 +0900

    마이페이지 메뉴

--- src/componets/mypage/mypage.jsx
+++ src/componets/mypage/mypage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './mypage.module.css'
 import CreatedClass from './components/createclass'
@@ -11,6 +11,12 @@ const MyPage = (props) => {
   const toAuth = () => {
     navigate('/instructor/auth')
   }
+  const [menu, setMenu] = useState('class-menu')
+
+  const onChange = (e) => {
+    const value = e.target.id
+    setMenu(value)
+  }
 
   return (
     <div className={styles.container}>
@@ -19,24 +25,65 @@ const MyPage = (props) => {
       </section>
 
       <div className={styles.class}>
-        {insId === 'null' ? (
-          <section className={styles.section}>
-            <div className={styles.title}>선생님 인증하기</div>
-            <p className={styles.auth} onClick={toAuth}>
-              나의 클래스를 창설하고 싶다면, 선생님 인증을 진행해 주세요 :)
-            </p>
-          </section>
+        <hgroup className={styles.radioGroup}>
+          <input
+            type='radio'
+            id='class-menu'
+            name='menu'
+            className={styles.radioForm}
+            defaultChecked={menu}
+            onChange={onChange}
+          ></input>
+          <label htmlFor='class-menu' className={styles.radioForm}>
+            클래스
+          </label>
+          <input
+            type='radio'
+            id='wish-menu'
+            name='menu'
+            className={styles.radioForm}
+            onChange={onChange}
+          ></input>
+          <label htmlFor='wish-menu' className={styles.radioForm}>
+            위시리스트
+          </label>
+          <input
+            type='radio'
+            id='mentor-menu'
+            name='menu'
+            className={styles.radioForm}
+            onChange={onChange}
+          ></input>
+          <label htmlFor='mentor-menu' className={styles.radioForm}>
+            멘토
+          </label>
+        </hgroup>
+
+        {menu === 'mentor-menu' ? (
+          insId === 'null' ? (
+            <section className={styles.section}>
+              <div className={styles.title}>선생님 인증하기</div>
+              <p className={styles.auth} onClick={toAuth}>
+                나의 클래스를 창설하고 싶다면, 선생님 인증을 진행해 주세요 :
+              </p>
+            </section>
+          ) : (
+            //멘토가 창설한 강의
+            <section className={styles.section}>
+              <CreatedClass />
+            </section>
+          )
         ) : (
-          //멘토가 창설한 강의
-          <section className={styles.section}>
-            <CreatedClass />
-          </section>
+          ''
         )}
 
-        {/* 수강중인 강의  */}
-        <section>
-          <TakeClass />
-        </section>
+        {menu === 'class-menu' ? (
+          <section>
+            <TakeClass />
+          </section>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   )
